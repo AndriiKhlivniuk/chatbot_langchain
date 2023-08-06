5d983f5290cee011acbb4c6e3172ddbdb39f20c5
andreykhlivniuk <khlivnia@tcd.ie>
2023-08-06 17:59:37 +0300

    Add .gitignore file

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
   <li>In the chatbot.py file insert your OPEAI token</li>
   <li>Run server(Flask): uvicorn server:app --host 0.0.0.0 --port 8000 </li>
   <li>In new terminal run client: python client.py</li>
-  <li>If you have the following error: You are using a deprecated configuration of Chroma. Please pip install chroma-migrate and run chroma-migrate to upgrade your configuration. See https://docs.trychroma.com/migration for more information or join our discord at https://discord.gg/8g5FESbj for help!\n
+  <li>If you have the following error: "You are using a deprecated configuration of Chroma. Please pip install chroma-migrate and run chroma-migrate to upgrade your configuration. See https://docs.trychroma.com/migration for more information or join our discord at https://discord.gg/8g5FESbj for help!"<br>
   Remove the line chroma_db_impl="duckdb+parquet", from langchain.vectorstores/chroma.py</li>
 </ol>  
 
--- chatbot.py
+++ chatbot.py
@@ -6,8 +6,10 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
 
-os.environ["OPENAI_API_KEY"] ="YOUR OPENAI API KEY"
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 pdf_path = "terms.pdf"
 loader = PyPDFLoader(pdf_path)
 pages = loader.load_and_split()
@@ -15,19 +17,12 @@ pages = loader.load_and_split()
 embeddings = OpenAIEmbeddings()
 vectordb = Chroma.from_documents(pages, embedding=embeddings, 
                                  persist_directory=".")
-# vectordb.persist()
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-pdf_qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.8, model_name='gpt-3.5-turbo') , vectordb.as_retriever(),  memory=memory)
+pdf_qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo') , vectordb.as_retriever(),  memory=memory)
 
 def conv(prompt):
-    no_ans = "\nGive the following  reply: 'I don't know please contact with support by email support@nifty-bridge.com' word by word, no changes if promt is not related to the context "
-    query = prompt+no_ans
+    query = prompt
     result = pdf_qa({"question": query})
-
-    # if this is the first message in the chat
-    if len(result["chat_history"])==2:
-        return "Hello I am NiftyBridge AI assistant. How could I help you?"
-    
     return result["answer"]
 
 
